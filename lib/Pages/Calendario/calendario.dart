2d24f3411d72a386649922a3e6b06790cee34952
Anabelly <anabellybel123@gmail.com>
2024-07-09 21:31:01 -0300

    funcao do calendario

--- lib/Pages/Calendario/calendario.dart
+++ lib/Pages/Calendario/calendario.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'dart:collection';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Calendario(),
+    );
+  }
+}
 
 class Calendario extends StatefulWidget {
   const Calendario({super.key});
@@ -10,6 +29,33 @@ class Calendario extends StatefulWidget {
 }
 
 class _CalendarioState extends State<Calendario> {
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  final LinkedHashMap<DateTime, List<Event>> events = LinkedHashMap(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,21 +67,121 @@ class _CalendarioState extends State<Calendario> {
             fontFamily: 'Roboto',
             fontWeight: FontWeight.bold,
             fontSize: 20,
-            color: Colors.white
+            color: Colors.white,
           ),
         ),
         centerTitle: true,
-         iconTheme: const IconThemeData(
-          color: Colors.white, 
-        ), 
-        elevation: 4.0, 
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        elevation: 4.0,
         shadowColor: Colors.black.withOpacity(0.5),
       ),
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color(0xFFC5CBFD),
+
+       margin: const EdgeInsets.symmetric(vertical: 100.0, horizontal: 16.0), 
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center, 
+        decoration: BoxDecoration(
+          color: Colors.white, 
+          borderRadius: BorderRadius.circular(16.0), 
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3), 
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                    _selectedEvents.value = _getEventsForDay(selectedDay);
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+              eventLoader: (day) {
+                return _getEventsForDay(day);
+              },
+              calendarBuilders: CalendarBuilders(
+                dowBuilder: (context, day) {
+                  if (day.weekday == DateTime.sunday) {
+                    final text = DateFormat.E().format(day);
+                    return Center(
+                      child: Text(
+                        text,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                    );
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                valueListenable: _selectedEvents,
+                builder: (context, value, _) {
+                  return ListView.builder(
+                    itemCount: value.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 4.0,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: ListTile(
+                          onTap: () => print('${value[index]}'),
+                          title: Text('${value[index]}'),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
+}
+
+class Event {
+  final String title;
+
+  Event(this.title);
+
+  @override
+  String toString() => title;
 }
