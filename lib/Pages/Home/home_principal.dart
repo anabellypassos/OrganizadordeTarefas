cf94e9719424872d281d23b643a797d802cb02c9
anabellypassos <anabellybel123@gmail.com>
2024-09-07 17:14:10 -0300

    tela home

--- lib/Pages/Home/home_principal.dart
+++ lib/Pages/Home/home_principal.dart
@@ -1,71 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:organizadortarefas/Pages/Calendario/calendario.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class HomePrincipal extends StatelessWidget {
+  const HomePrincipal({super.key});
 
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFC5CBFD),
-        iconTheme: const IconThemeData(color: Colors.white),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const ListTile(
-              leading: Icon(Icons.home_outlined, color: Color.fromARGB(255, 49, 49, 49)), 
-              title: Text('Menu', style: TextStyle(color:Color.fromARGB(255, 49, 49, 49))),
-            ),
-            ListTile(
-              leading: const Icon(Icons.date_range_outlined, color: Color.fromARGB(255, 49, 49, 49)),
-              title: const Text('Calendário', style: TextStyle(color: Color.fromARGB(255, 49, 49, 49))),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Calendario()),
-                );
-              },
-            ),
-            const ListTile(
-              leading: Icon(Icons.view_agenda_outlined, color: Color.fromARGB(255, 49, 49, 49)), 
-              title: Text('Agenda', style: TextStyle(color:Color.fromARGB(255, 49, 49, 49))),
-            ),
-            const ListTile(
-              leading: Icon(Icons.wb_sunny_outlined, color: Color.fromARGB(255, 49, 49, 49)), 
-              title: Text('Organizador diário', style: TextStyle(color:Color.fromARGB(255, 49, 49, 49))),
-            ),
-            const ListTile(
-              leading: Icon(Icons.calendar_today, color: Color.fromARGB(255, 49, 49, 49)),
-              title: Text('Organizador semanal', style: TextStyle(color:Color.fromARGB(255, 49, 49, 49))),
-            ),
-            const ListTile(
-              leading: Icon(Icons.assignment_outlined, color: Color.fromARGB(255, 49, 49, 49)), 
-              title: Text('Anotações', style: TextStyle(color:Color.fromARGB(255, 49, 49, 49))),
-            ),
-            const Divider(),
-            const SizedBox(height: 20),
-            Image.asset('assets/image/nave.png'), // Ajuste o caminho da imagem conforme necessário
-          ],
-        ),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color(0xFFC5CBFD),
-        child: Center(
-          child: Image.asset('assets/image/menu.png',
-          fit: BoxFit.contain, 
+        appBar: AppBar(
+          title:Text(
+            'Minhas Tarefas',
+          style: TextStyle(
+            color:Colors.purple[800],
           ),
-           
+          ),
+         
+        
         ),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined),
+            label: "Calendario",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task_alt_outlined),
+            label: "Nova Tarefa",
+          ),
+        ]),
+        body: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.topLeft,
+                stops: [
+                  0.1,
+                  0.4,
+                  0.6,
+                  0.9,
+                ],
+                colors: [
+                  Color.fromARGB(255, 60, 185, 235),
+                  Color.fromARGB(255, 227, 106, 249),
+                  Color.fromARGB(255, 247, 115, 225),
+                  Color.fromARGB(255, 144, 67, 238),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'Pages/Home/home_principal.dart';
 
 void main() {
-  runApp(const Inicio());
+  runApp(const MyApp());
 }
 
-class Inicio extends StatelessWidget {
-  const Inicio({super.key});
+// Definição da classe MyApp
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,76 +16,10 @@ class Inicio extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color(0xFFC5CBFD),
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Seja bem vindo(a), \n  ao Meu \n Organizador de \n Tarefas',
-              style: TextStyle(
-                fontSize: 34,
-                color: Colors.white,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.left,
-            ),
-            const SizedBox(height: 20),
-            Image.asset(
-              'assets/image/logo.png',
-              width: 250,
-              height: 250,
-            ),
-            const SizedBox(height: 51),
-            SizedBox(
-              width: 252,
-              height: 53,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Home()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFFCC65C),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    side: const BorderSide(
-                      color: Color(0xFF2C2C2C),
-                      width: 1,
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Ir para o menu',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePrincipal(), // Corrigir aqui: Retorna a instância do widget
+      },
     );
   }
 }
